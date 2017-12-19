78807a13299412571ce1f6b1815486af9d3e13f8
RITHIKSANJEEV97 <34641065+RITHIKSANJEEV97@users.noreply.github.com>
2017-12-19 10:54:26 +0530

    Create postivenegativezero.c

---
+++ beginner/set-1/postivenegativezero.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) {
+int n;
+scanf("%d",&n);
+if(n!=0)
+{
+if(n>0)
+{
+printf("positive");
+}else
+{
+printf("negative");
+}
+}
+else
+{
+printf("zero");
+}
+return 0;
+}
+
+
