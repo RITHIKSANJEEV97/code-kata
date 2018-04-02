2df2c88edfcba76b4d2c4a9bb0cc218f41fd30ac
RITHIKSANJEEV97 <34641065+RITHIKSANJEEV97@users.noreply.github.com>
2018-04-02 14:34:06 +0530

    Create cor106.java

---
+++ cor106.java
@@ -0,0 +1,13 @@
+package set11;
+import java.io.*;
+public class Correspondingchar105 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int a=Integer.parseInt(br.readLine());
+		char c='A';
+		System.out.print((char) (b+(a-1)));
+	}
+
+}
