e23c02a6599713ef174f1d1aae1d04f85d5689c6
RITHIKSANJEEV97 <34641065+RITHIKSANJEEV97@users.noreply.github.com>
2018-04-02 14:54:43 +0530

    Create le45.java

---
+++ beginner/set-5/le45.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class le {
+     public static void main(String aa[]){
+     int a,f=0;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     String c=String.valueOf(a);
+     int l=c.length();
+     System.out.print(l);
+     
+
+    }
+}
